983e9a349fa54482280b6b9e133c9e4ad1fc6626
zorzet <44205109+zorzet@users.noreply.github.com>
2020-02-17 09:08:17 +0200

    Add files via upload

---
+++ target/src/main/java/gr/aueb/mscis/sample/model/Customer.java
@@ -0,0 +1,61 @@
+package gr.aueb.mscis.sample.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer { 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "ADT", length = 9, nullable = false)
+	private String ADT;
+	
+	
+	@Column(name="AFM", nullable = false)
+	private String AFM;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getADT() {
+		return ADT;
+	}
+
+	public void setADT(String aDT) {
+		ADT = aDT;
+	}
+
+	public String getAFM() {
+		return AFM;
+	}
+
+	public void setAFM(String aFM) {
+		AFM = aFM;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Column(name = "date", nullable = false)
+	private Date date;
+}
---
+++ target/src/main/java/gr/aueb/mscis/sample/model/Xartofulakio.java
@@ -0,0 +1,39 @@
+package gr.aueb.mscis.sample.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.*;
+
+@Entity
+@Table(name="Xartofulakio")
+public class Xartofulakio {
+  
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTrexousathesi() {
+		return trexousathesi;
+	}
+
+	public void setTrexousathesi(String trexousathesi) {
+		this.trexousathesi = trexousathesi;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "trexousathesi", length = 512, nullable = false)
+	private String trexousathesi;
+
+
+}
