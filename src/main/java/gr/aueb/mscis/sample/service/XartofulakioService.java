4d303a0526a63f439f1ba8650fa84c6e80419bd2
Georgia <Georgia@172.16.14.190>
2020-02-21 16:54:41 +0200

    service upo ulopoihsh apo zorzet

--- src/main/java/gr/aueb/mscis/sample/service/XartofulakioService.java
+++ src/main/java/gr/aueb/mscis/sample/service/XartofulakioService.java
@@ -1,10 +1,13 @@
 package gr.aueb.mscis.sample.service;
 
+/* Περίπτωση Χρήσης 6 */ 
+
+
+
 import java.util.List;
-
 import javax.persistence.EntityManager;
-
 import gr.aueb.mscis.sample.model.Xartofulakio;
+
 
 
 public class XartofulakioService {
@@ -14,19 +17,27 @@ public class XartofulakioService {
 		this.em = em;
 	}
 	
+	
+	
 /*vres ta xartofulakia pou diaxeirizetai o ka8e DX*/	
 	
 	@SuppressWarnings("unchecked")
 	public List<Xartofulakio> findXartofulakioByLastName(int DXId) {
 
-		List< Xartofulakio> results = null;
-/*		results = em
+		List< Xartofulakio> results = null;			
+		results = em
 				.createQuery(
-						"select b from Borrower b where b.person.lastName like :surname ")
-				.setParameter("surname", last_name).getResultList();
-*/				
+						"select x from Xartofulakio x where x.XId like :DXId ")
+						.setParameter("DXId", DXId).getResultList();
 		return results;
 	}
 
+/*gurise ta stoixeia pou exeis gia sugkekrimeno xartofulakio*/
+/*gia ena xartofulakio gurna onomatepwnumo, tis closed trasactions kai tis open*/
 
+
+
+
+
+/*enhmerwse ta stoixeia enos sugkekrimenou xartofulakiou*/
 }
