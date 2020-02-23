fb52f0b4559dd7e7e2226e3d442b14cbe5c21076
melina.raptaki <melina.raptaki@CPX-1X4NLY7TVZG>
2020-02-23 20:58:38 +0200

    UpdateIndicatorsTest

---
+++ src/test/java/gr/aueb/mscis/sample/service/UpdateIndicatorsTest.java
@@ -0,0 +1,38 @@
+package gr.aueb.mscis.sample.service;
+
+import static org.junit.Assert.*;
+import javax.persistence.*;
+import org.junit.*;
+
+import gr.aueb.mscis.sample.persistence.Initializer;
+import gr.aueb.mscis.sample.persistence.JPAUtil;
+import gr.aueb.mscis.sample.service.UpdateIndicatorsService;
+
+public class UpdateIndicatorsTest {
+	
+	protected EntityManager em;
+	
+	@Before
+	public void setup(){
+		Initializer dataHelper = new Initializer();
+		em = JPAUtil.getCurrentEntityManager();
+		dataHelper.prepareData();
+	}
+	
+	@After
+	public void tearDown(){
+		em.close();
+	}
+	
+	@Test
+	public void test_StockMissing() {
+		String exp_message = "NO RECORDS FOUND";
+		UpdateIndicatorsService update = new UpdateIndicatorsService(em);
+		try {
+			update.GetLast14ClosingsOfStock(99999);
+		} catch(java.lang.RuntimeException message) {
+			assertEquals(exp_message, message.getMessage());
+		}
+	}
+
+}
