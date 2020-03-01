1014eda8b063a4132d95fcc7de5c4589327ddd67
zorzet <44205109+zorzet@users.noreply.github.com>
2020-03-01 15:29:56 +0200

    Add files via upload

--- src/main/java/gr/aueb/mscis/sample/service/EvaluateFuturePositionsService.java
+++ src/main/java/gr/aueb/mscis/sample/service/EvaluateFuturePositionsService.java
@@ -1,17 +1,15 @@
 package gr.aueb.mscis.sample.service;
- 
+
+/* Περίπτωση Χρήσης 3 */
+
 import java.util.List;
 import javax.persistence.EntityManager;
 import gr.aueb.mscis.sample.model.Metoxh;
 import gr.aueb.mscis.sample.model.Transaction;
-import gr.aueb.mscis.sample.model.Customer;
 import gr.aueb.mscis.sample.model.Deiktes;
-import gr.aueb.mscis.sample.service.XartofulakioService;
 import gr.aueb.mscis.sample.model.Xartofulakio;
-import gr.aueb.mscis.sample.persistence.JPAUtil;
 
 
-/* Περίπτωση Χρήσης 3 */
 public class EvaluateFuturePositionsService {
 
 	EntityManager em;
@@ -20,12 +18,18 @@ public class EvaluateFuturePositionsService {
 		this.em = em;
 	}
 	
+/////////////////////////////////////////////////////////////////////
+//Το σύστημα εμφανίζει για τη μετοχή τους δείκτες (ΜΚΟ15, ΜΚΟ80, yk20, xk20, beta)
+/////////////////////////////////////////////////////////////////	
 	
-	/*Vres oles tis metoxes*/
+	/**
+	 *Επιστρέφει όλες τις μετοχές που παρακολουθούμε στη βάση μας 
+	 * @return List
+	 */
 	public List<Metoxh> findAllMotoxes() {
 		List<Metoxh> results = null;
 
-		results = em.createQuery("select from Metoxh m", Metoxh.class)
+		results = em.createQuery("select DISTINCT(m.Name) from Metoxh m", Metoxh.class)
 				.getResultList();
 		
 		if(results==null) {
@@ -34,45 +38,79 @@ public class EvaluateFuturePositionsService {
 		return results;
 	}
 	
-	/*Emfanise oles tis metoxes*/
-	public void showStocks() {
-		List <Metoxh> list=findAllMotoxes();
-        for(Metoxh metoxh : list) {
-            System.out.println(metoxh.getName()+" "+metoxh.getClosing()+" "+metoxh.getBeta()+" "+metoxh.getDate()+
-            		           " "+metoxh.getHigh()+" "+metoxh.getLow());
-        }
+	/**
+	 * Για μια μετοχή, επιστρέφει την εικόνα της για μια συγκεκριμένη ημερομηνία
+	 * @param name
+	 * @param date
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Metoxh InformationOfStock(String name, String date) {
+		String InformationOfStock=null;
+		List<Metoxh> ml=null;
+		ml=findAllMotoxes();
+	    for (int i = 0; i < ml.size(); i++) {
+	    	if(ml.get(i).getDate().equalsIgnoreCase(date)&(ml.get(i).getName().equalsIgnoreCase(name))) {
+	    		return ml.get(i);
+	    	}
+	    }
+	    
+	    	throw new java.lang.RuntimeException("NO STOCK FOUND");
 	}
 	
-	/*vres mia metoxh */
-	public Metoxh findMetoxh(int StockId) {
-		Metoxh results=null;
-		results =(Metoxh) em.createQuery("select m from Metoxh m where m.Stockid : stockid")
-				.setParameter("sotckid", StockId).getSingleResult();		
-		if(results==null) {
-			throw new java.lang.RuntimeException("NO STOCK FOUND");
-		}
-		return results;
-	}
 	
-	
-	/*Gia mia metoxh emfanise olous tous deiktes*/
-	public Deiktes findDeiktesPerStock(int StockId){
+	/**
+	 * Γυρνάει την εικόνα των δεικτών για μια συγκεκριμένη μετοχή
+	 * για μια συγκεκριμένη μέρα
+	 * @param name
+	 * @param date
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public Deiktes findDeiktesPerStock(String name, String date){
+		List <Deiktes>  dlist=null;
 		Deiktes d=null;
-		d=(Deiktes)em.createQuery("select d from Deiktes d where d.metoxh.stockId like :metoxh ")
-				.setParameter("metoxh",StockId).getSingleResult();
+		Metoxh m=InformationOfStock(name,date);
+		dlist=em.createQuery("select d from Deiktes d where d.metoxh.Name like :metoxh ")
+				.setParameter("metoxh",name).getResultList();
+		for(int i=0; i<dlist.size(); i++)
+			{
+				if(dlist.get(i).getDate().equalsIgnoreCase(m.getDate()))
+						d=dlist.get(i);
+			}
 		if(d==null) {
 			throw new java.lang.RuntimeException("NO DEIKTES RECORD FOUND");
 		}
 		return d;
 	}
 	
-	/*Gia mia metoxh kane protash PPO me vash tous deiktes*/
-	public String BuyOrSellPerStock(int StockId) {
+	public String StockImage(String name, String date) {
+	String InformationOfStock; 
+	Metoxh m=null;
+	Deiktes d=null;
+	m=InformationOfStock(name,date);
+	d=findDeiktesPerStock(name,date);
+	InformationOfStock=" Name "+m.getName()+" Date "+m.getDate()+" Beta "+m.getBeta()+m.getClosing()
+						+ " MKO15 "+d.getMKO15()+" MKO80: "+d.getMKO80()+" XK20 "+d.getXk20()+" YK2O "+d.getYk20();
+	
+	return InformationOfStock;
+	}
+/////////////////////////////////////////////////////////////////////
+//Το σύστημα εμφανίζει πρόταση με βάση τον αλγόριθμο PPO.
+/////////////////////////////////////////////////////////////////	
+	/**
+	 * Για μια μτοχή, για κάποια ημέρα, με βάση τη τιμή της μετοχής και τους δείκτες, υπολογίζεται ο PPO
+	 * και επιστρέφει μήνυμα αγοράς η πώλησης
+	 * @param sname
+	 * @param date
+	 * @return
+	 */
+	public String BuyOrSellPerStock(String sname, String date) {
 		String decision=null;
 		Deiktes d=null;
 		Metoxh m=null;
-		m=findMetoxh(StockId);
-		d=findDeiktesPerStock(StockId);
+		m=InformationOfStock(sname,date);
+		d=findDeiktesPerStock(sname,date);
 		if(((d.getMKO15()>d.getMKO80())&(m.getClosing()>d.getYk20()))){
 			decision="buy";
 		}else if(m.getClosing()<d.getXk20()){
@@ -82,10 +120,21 @@ public class EvaluateFuturePositionsService {
 		}
 		return decision;
 	}	
-		
-	/*Emfanise Plhorofories Metoxhs sto Xarofulakio- an uparxei transaction open*/
+/////////////////////////////////////////////////////////////////////
+//Το σύστημα εμφανίζει για τη συγκεκριμένη μετοχή τα τεμάχια που έχει ο χρήστης,
+//το ποσοστό του κεφαλαίου που καταλαμβάνουν και το beta του χαρτολακίου.
+/////////////////////////////////////////////////////////////////			
+	
+	/**
+	 * Επιστρέφει για τη συγκεκριμένη μετοχή τα τεμάχια που έχει ο χρήστης
+	 * @param Stock
+	 * @param XId
+	 * @param CusId
+	 * @return
+	 */
 	@SuppressWarnings("unchecked")
-	public void showUnitsofStocksperPortofolio(String Stock, int XId,int CusId) {
+	public int showUnitsofStocksperPortofolio(String Stock, int XId,int CusId) {
+		int units=0;
 		//PARE OLA TA XARTOFULAKIA TOU DX
 		List<Xartofulakio> xlist=null;
 		List<Transaction> translist=null;
@@ -102,10 +151,32 @@ public class EvaluateFuturePositionsService {
 		//VRES TA TRANSACTION POU EGINAN GIA AUTH TH METOXH & EINAI OPEN
 		for (int j = 0; j < translist.size(); j++) {
 			if((translist.get(j).getState().equalsIgnoreCase("Open")&(translist.get(j).getStock().equalsIgnoreCase(Stock)))){
-				System.out.println("Stock in Portofolio "+translist.get(j).getStock()+" Units "+translist.get(j).getUnits()
-						+"Bought at Price "+translist.get(j).getPrice()+" at date "+translist.get(j).getDate());
+				units=translist.get(j).getUnits();
 			}
 		}
+		return units;
 	}
-
+	@SuppressWarnings("unchecked")
+	public double showPosostoofStocksperPortofolio(String Stockn, int XId,int CusId,String date) {
+		double pososto=0.0;
+		//PARE OLA TA XARTOFULAKIA TOU DX
+		List<Xartofulakio> xlist=null;
+		List<Transaction> translist=null;
+		xlist= em.createQuery("select x from Xartofulakio x where x.XId like :DXId ").setParameter("DXId", XId)
+				.getResultList();
+		//VRES AUTO TOU PELATH
+		Xartofulakio x=null;
+		for (int i = 0; i < xlist.size(); i++) {
+			if((xlist.get(i).getCus().getCustomerId())==CusId) {
+				pososto=xlist.get(i).getCus().getInvestAmount();
+			}
+		}
+		pososto=(showUnitsofStocksperPortofolio(Stockn,XId,CusId)*InformationOfStock(Stockn,date).getClosing() )/pososto;
+		return pososto;
+	}
+	public void printing(String Stockn, int XId,int CusId,String date) {
+		System.out.println("UNITS OF STOCK IN PORTOFOLIO "+showUnitsofStocksperPortofolio(Stockn,XId,CusId));
+		System.out.println("Percentage of Units of Stock In Portofolio "+showPosostoofStocksperPortofolio(Stockn,XId,CusId,date));
+		System.out.println("BETA of Portofolio "+gr.aueb.mscis.sample.service.XartofulakioService.CalculateBeta( XId,CusId,date));
+	}
 }
