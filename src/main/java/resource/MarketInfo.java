20557c6eff10e09e95f770e09b185c02201a6fcd
Zorzet <Zorzet@LAPTOP-LSR5HSV5>
2020-03-22 15:09:11 +0200

    added ShowMarketResource

---
+++ src/main/java/resource/MarketInfo.java
@@ -0,0 +1,27 @@
+package resource;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import gr.aueb.mscis.sample.model.Metoxh;
+
+@XmlRootElement
+public class MarketInfo {
+	private String marketInfo;
+
+	public String getMarketInfo() {
+		return marketInfo;
+	}
+
+	public void setMarketInfo(String marketInfo) {
+		this.marketInfo = marketInfo;
+	}
+	
+	public MarketInfo(String marketInfo) {
+		super();
+		this.marketInfo = marketInfo;
+	}
+
+	public static  MarketInfo wrap(String info) {
+		return new  MarketInfo(info);
+	}
+}
---
+++ src/main/java/resource/ShowMarketResource.java
@@ -0,0 +1,72 @@
+package resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import gr.aueb.mscis.sample.service.ShowMarketService;
+import gr.aueb.mscis.sample.model.MarketsData;
+import gr.aueb.mscis.sample.persistence.JPAUtil;
+import gr.aueb.mscis.sample.service.EvaluateFuturePositionsService;
+
+@Path("EvaluatePositions")
+public class ShowMarketResource {
+
+	@Context
+	UriInfo uriInfo;
+	
+	protected EntityManager getEntityManager() {
+
+		return JPAUtil.getCurrentEntityManager();
+	}	
+/////////////////////////////////////////////////////////////////////
+//Το σύστημα εμφανίζει την εικόνα του FTSE εκείνη τη στιγμή 
+/////////////////////////////////////////////////////////////////	
+	@GET
+	@Path("getOnlineMarket")
+	@Produces(MediaType.APPLICATION_JSON)
+	public MarketInfo  getOnlineMarketImage() {
+		EntityManager em = getEntityManager();
+		MarketInfo marketsnow=null;
+		ShowMarketService s = new ShowMarketService(em);
+		return MarketInfo.wrap(s.getOnlineMarketImage());
+	}
+/////////////////////////////////////////////////////////////////////
+//Εμφανίζεται κατάλληλο μήνυμα ενημέρωσης
+//Εμφανίζεται η εικόνα του FTSE για όλη την προηγούμενη εργάσιμη και η τιμή κλεισίματος.
+////////////////////////////////////////////////////////////////////	
+	@GET
+	@Path("getOffineMarket")
+	@Produces(MediaType.APPLICATION_JSON)
+	public MarketInfo getOfflineMarketImage() {
+		MarketInfo info;
+		EntityManager em = getEntityManager();
+		ShowMarketService s = new ShowMarketService(em);
+		info=MarketInfo.wrap(s.getOfflineMarketImage());
+		return info;
+	}
+/////////////////////////////////////////////////////////////////////
+//Το σύστημα εμφανίζει τα κλεισίματα του FTSE για το επιλεγμένο διάστηματος.
+////////////////////////////////////////////////////////////////////
+	@GET
+	@Path("ShowMarketHistory/{daysno}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<MarketsDataInfo> ShowMarketHistory(@PathParam("daysno") int daysno){
+		EntityManager em = getEntityManager();
+		List<MarketsData> list=null;
+		List<MarketsDataInfo> outputlist=new ArrayList<MarketsDataInfo>();
+		ShowMarketService s = new ShowMarketService(em);
+		list=s.ShowMarketHistory(daysno);
+		for(MarketsData mdi:list ) {
+			outputlist.add(MarketsDataInfo.wrap(mdi));
+		}
+		return outputlist;
+	}
+}
