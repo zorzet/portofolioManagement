2f887b0508feeabad384e2d7f2107303ef351ca0
Zorzet <Zorzet@192.168.56.1>
2020-03-28 00:52:48 +0200

    Added Resource for XartofulakioService

---
+++ src/main/java/gr/aueb/mscis/sample/resource/XartofulakioResource.java
@@ -0,0 +1,103 @@
+package gr.aueb.mscis.sample.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import gr.aueb.mscis.sample.service.XartofulakioService;
+import gr.aueb.mscis.sample.model.Transaction;
+import gr.aueb.mscis.sample.model.Xartofulakio;
+import gr.aueb.mscis.sample.persistence.JPAUtil;
+
+
+@Path("Xartofulakio")
+public class XartofulakioResource {
+
+	@Context
+	UriInfo uriInfo;
+	
+	protected EntityManager getEntityManager() {
+
+		return JPAUtil.getCurrentEntityManager();
+	}
+	
+/////////////////////////////////////////////////////////////
+//// Για ένα Διαχειριστή επιστρέφει μια λίστα με τα χαρτοφυλάκια που αυτός διαχειρίζεται
+//////////////////////////////////////////////////////////	
+	@GET
+	@Path("xartofulakia/{DXId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public List <XartofulakioInfo> findXartofulakiaById(@PathParam("DXId") int  DXId) {
+		EntityManager em = getEntityManager();
+		List<XartofulakioInfo> outputlist=new ArrayList<XartofulakioInfo>();
+		List<Xartofulakio> list=null;
+		try {
+			XartofulakioService x = new XartofulakioService(em);
+			list=x.findXartofulakiaById(DXId);
+			for(Xartofulakio xartofulakio:list ) {
+				outputlist.add(XartofulakioInfo.wrap(xartofulakio));
+			}
+			return outputlist;
+		}catch(Exception e) {
+			return null;
+		}
+	}
+
+///////////////////////////////////////////////////////////////////////////////////////////////	
+//Το σύστημα εμφανίζει το επιλεγμένο χαρτοφυλάκιο του πελάτη (τρέχουσες θέσεις, beta, ιστορικό).	
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+	@GET
+	@Path("xartofulakioPelath/{DXId}/{Cusid}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String ShowXartofulakioPelath(@PathParam("DXId") int DXId,@PathParam("Cusid") int Cusid) {
+		String response=null;
+		EntityManager em = getEntityManager();
+		XartofulakioService x = new XartofulakioService(em);
+		try {
+			response=x.ShowXartofulakioPelath(DXId,Cusid);
+		}catch (Exception e) {
+			
+		}
+	return response;
+	}
+
+///////////////////////////////////////////////////////////////////////////////////////////////	
+//Το σύστημα εμφανίζει για το επιλεγμένο Χαρτοφυλάκιο αν είναι αμυντικό, επιθετικό ή ουδέτερο       
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@GET
+	@Path("xartofulakioPelath/{DXId}/{Cusid}/{date}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String ReturnCustomerImage(@PathParam("DXId") int DXId,@PathParam("Cusid") int Cusid, @PathParam("date") String date) {
+		String response=null;
+		EntityManager em = getEntityManager();
+		XartofulakioService x = new XartofulakioService(em);
+		try {
+			response=x.ReturnCustomerImage(DXId, Cusid, date);
+		}catch (Exception e) {
+
+		}
+	return response;	
+	}
+	
+//////////////////////////////////////////////////////////////////////////////////////////////////
+////Ενημέρωση Θέσης Πελάτη
+/////////////////////////////////////////////////////////////////////////////////////////////////		
+    @POST
+    @Path("/transact/{cmdType: [A-Za-z]*}/{stock: [A-Za-z]*}/{units}/{price}/{date}/{state: [A-Za-z]*}/{x}")
+	@Consumes({"text/plain"})
+	public void transact(@PathParam("cmdType") String cmdType,@PathParam("stock") String stock,@PathParam("units") int units
+			,@PathParam("price") double price,@PathParam("date") String date,@PathParam("state") String state,
+			@PathParam("x") Xartofulakio x) {	
+    		
+    }
+  }
